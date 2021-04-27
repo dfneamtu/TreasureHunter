c26e37c9079ad462432e38264cf190ed40dfbb94
Tommy Rennolds <thomasrennolds@gmail.com>
2021-04-27 17:34:29 -0400

    update PlayersTickets class to make ticket variables more reader friendly

--- Assets/Scripts/PlayersTickets.cs
+++ Assets/Scripts/PlayersTickets.cs
@@ -8,15 +8,22 @@ using Random=UnityEngine.Random;
 public class PlayersTickets : MonoBehaviour
 {
 	public TMP_Text[] ticketTexts = new TMP_Text[4];
-	public int[] ticketValues = new int[4];
+	public int boatTickets;
+    public int planeTickets;
+    public int roadTickets;
+    public int trainTickets;
+
     // Start is called before the first frame update
     void Start()
     {
+        boatTickets = 0;
+        planeTickets = 0;
+        roadTickets = 0;
+        trainTickets = 0;
         for (int i = 0; i < 4; i++) 
         {
         	ticketTexts[i] = this.gameObject.transform.GetChild(i).GetComponent<TMP_Text>();
         	ticketTexts[i].text = "0";
-        	ticketValues[i] = 0;
         }
     }
 
@@ -26,11 +33,28 @@ public class PlayersTickets : MonoBehaviour
     	while (ticketGained == 0) 
     	{
     		ticketGained = ML_Algo.ML();
-            Debug.Log(ticketGained.ToString());
     	}
 
-    	ticketValues[ticketGained - 1] += 1;
-    	ticketTexts[ticketGained - 1].text = ticketValues[ticketGained - 1].ToString();
+        if (ticketGained == 1) 
+        {
+            roadTickets++;
+            ticketTexts[3].text = roadTickets.ToString();
+        }
+        else if (ticketGained == 2)
+        {
+            trainTickets++;
+            ticketTexts[2].text = trainTickets.ToString();
+        }
+        else if (ticketGained == 3) 
+        {
+            boatTickets++;
+            ticketTexts[0].text = boatTickets.ToString();
+        }
+        else
+        {
+            planeTickets++;
+            ticketTexts[1].text = planeTickets.ToString();
+        }
     }
 
     void Update()
