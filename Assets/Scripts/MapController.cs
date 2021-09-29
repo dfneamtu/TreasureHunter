4d668e842d1aa1a6f80f51315c58b85ac2e6aafc
Tommy Rennolds <thomasrennolds@gmail.com>
2021-09-29 16:39:34 -0400

    enemies are spawnign with placeholder art

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+
 public class MapController : MonoBehaviour
 {
 
@@ -13,6 +14,8 @@ public class MapController : MonoBehaviour
 
     public int currentHub;
 
+    public GameObject enemyObjPrefab;
+
     public Material Hub1;
     public Material Hub2;
     public Material Hub3;
@@ -21,7 +24,9 @@ public class MapController : MonoBehaviour
     private ReadLocations locationsScript;
     public GameObject LocationsObj;
     public List<Location> locations = new List<Location>();
+    public List<Enemy> enemies = new List<Enemy>();
     public GameObject[] players = new GameObject[6];
+    public List<GameObject> enemiesObj = new List<GameObject>();
     public Vector3 vector;
     public Vector3 destination;
     public int turn;
@@ -39,7 +44,7 @@ public class MapController : MonoBehaviour
     {
         //m_someOtherScriptOnAnotherGameObject = GameObject.FindObjectOfType(typeof(ScriptA)) as ScriptA;
 
-
+        enemies = GlobalController.Instance.enemies;
         startOfTurn = GlobalController.Instance.startOfTurn;
         turn = GameController.playerTurn;
 
@@ -68,7 +73,7 @@ public class MapController : MonoBehaviour
         }
 
 
-
+        //check if the current player has a commited location, if so, commit travel
         if (startOfTurn)
         {
           if (hubLocation[turn - 1] != newHub[turn - 1] || pLocation[turn - 1] != newLocation[turn -1])
@@ -100,6 +105,7 @@ public class MapController : MonoBehaviour
         GlobalController.Instance.startOfTurn = startOfTurn;
       }
 
+      //load all players to their current location
       for (int i = 0; i < 6; i++)
       {
         foreach(Location l in locations)
@@ -116,7 +122,7 @@ public class MapController : MonoBehaviour
         }
       }
 
-
+      //move any players not in the current hub off the screen
       for (int i = 0; i < 6; i++)
       {
         if (hubLocation[i] != currentHub)
@@ -124,8 +130,26 @@ public class MapController : MonoBehaviour
           players[i].transform.position = new Vector3(2000,2000,2000);
         }
       }
+
+      //add enemies to Screen
+      foreach(Enemy e in enemies)
+      {
+        foreach(Location l in locations)
+        {
+          if (e.hubNum == l.hubNum && e.locationNum == l.locationNum && e.hubNum == currentHub)
+          {
+            spawnEnemy(l.xPos, l.zPos);
+          }
+        }
+      }
     }
 
+
+    private void spawnEnemy(float xPos, float zPos)
+    {
+      GameObject a = Instantiate(enemyObjPrefab) as GameObject;
+      a.transform.position = new Vector3(xPos, 0, zPos);
+    }
     // Update is called once per frame
     void Update()
     {
