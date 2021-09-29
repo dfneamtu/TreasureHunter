2c3d9075c98a9d8e78781d6a35bf14688d0b5782
Tommy Rennolds <thomasrennolds@gmail.com>
2021-09-28 23:20:50 -0400

    6 empty player objects

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -11,6 +11,8 @@ public class MapController : MonoBehaviour
     public GameObject LocationsObj;
     public List<Location> locations = new List<Location>();
     public GameObject Cube;
+    public Vector3 vector;
+    public Vector3 destination;
 
     void Awake()
     {
@@ -22,8 +24,14 @@ public class MapController : MonoBehaviour
         hubLocation = GlobalController.Instance.hubLocation;
         pLocation = GlobalController.Instance.pLocation;
         locations = locationsScript.locations;
+        vector = new Vector3(locations[2].xPos, 0, locations[2].zPos);
+        destination = new Vector3(locations[0].xPos, 0, locations[0].zPos);
 
-        Cube.transform.position = new Vector3(locations[2].xPos, 0, locations[2].zPos);
+        Cube.transform.position = vector;
+        Vector3 final = Vector3.MoveTowards(vector, destination, Time.deltaTime * 50f);
+        Cube.transform.position = final;
+        //Cube.transform.position = vector;
+        //Cube.transform.position = Vector3.MoveTowards(Cube.transform.position, destination, Time.deltaTime * speed);
 
     }
 
