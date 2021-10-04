1d1651165c8ee0273a3a29ff0d0fdc2c988628cc
dfneamtu <59751357+dfneamtu@users.noreply.github.com>
2021-10-03 21:59:02 -0400

    3dMapErrorFixed

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -6,6 +6,11 @@ using Random = UnityEngine.Random;
 
 public class MapController : MonoBehaviour
 {
+    public GameObject[] turnHighlighter;
+    public GameObject[] playerPrefabs;
+    public GameObject[] playerTokens;
+
+    public int totalPlayers;
 
     public int[] hubLocation = new int[6];
     public int[] pLocation = new int[6];
@@ -41,6 +46,9 @@ public class MapController : MonoBehaviour
     void Start()
     {
         //m_someOtherScriptOnAnotherGameObject = GameObject.FindObjectOfType(typeof(ScriptA)) as ScriptA;
+
+
+        totalPlayers = GameController.maxPlayers;
 
         enemies = GlobalController.Instance.enemies;
         startOfTurn = GlobalController.Instance.startOfTurn;
@@ -112,6 +120,155 @@ public class MapController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (turn)
+        {
 
+            case 1:
+                turnHighlighter[0].gameObject.SetActive(true);
+                turnHighlighter[1].gameObject.SetActive(false);
+                turnHighlighter[2].gameObject.SetActive(false);
+                turnHighlighter[3].gameObject.SetActive(false);
+                turnHighlighter[4].gameObject.SetActive(false);
+                turnHighlighter[5].gameObject.SetActive(false);
+
+                break;
+
+            case 2:
+
+                turnHighlighter[0].gameObject.SetActive(false);
+                turnHighlighter[1].gameObject.SetActive(true);
+                turnHighlighter[2].gameObject.SetActive(false);
+                turnHighlighter[3].gameObject.SetActive(false);
+                turnHighlighter[4].gameObject.SetActive(false);
+                turnHighlighter[5].gameObject.SetActive(false);
+
+                break;
+            case 3:
+                turnHighlighter[0].gameObject.SetActive(false);
+                turnHighlighter[1].gameObject.SetActive(false);
+                turnHighlighter[2].gameObject.SetActive(true);
+                turnHighlighter[3].gameObject.SetActive(false);
+                turnHighlighter[4].gameObject.SetActive(false);
+                turnHighlighter[5].gameObject.SetActive(false);
+
+                break;
+            case 4:
+
+                turnHighlighter[0].gameObject.SetActive(false);
+                turnHighlighter[1].gameObject.SetActive(false);
+                turnHighlighter[2].gameObject.SetActive(false);
+                turnHighlighter[3].gameObject.SetActive(true);
+                turnHighlighter[4].gameObject.SetActive(false);
+                turnHighlighter[5].gameObject.SetActive(false);
+
+                break;
+            case 5:
+                turnHighlighter[0].gameObject.SetActive(false);
+                turnHighlighter[1].gameObject.SetActive(false);
+                turnHighlighter[2].gameObject.SetActive(false);
+                turnHighlighter[3].gameObject.SetActive(false);
+                turnHighlighter[4].gameObject.SetActive(true);
+                turnHighlighter[5].gameObject.SetActive(false);
+
+                break;
+
+            case 6:
+                turnHighlighter[0].gameObject.SetActive(false);
+                turnHighlighter[1].gameObject.SetActive(false);
+                turnHighlighter[2].gameObject.SetActive(false);
+                turnHighlighter[3].gameObject.SetActive(false);
+                turnHighlighter[4].gameObject.SetActive(false);
+                turnHighlighter[5].gameObject.SetActive(true);
+
+                break;
+
+            default:
+                //Console.WriteLine("Default case");
+                break;
+
+        }
+
+
+        if (totalPlayers == 2)
+        {
+            playerPrefabs[0].gameObject.SetActive(true);
+            playerPrefabs[1].gameObject.SetActive(true);
+            playerPrefabs[2].gameObject.SetActive(false);
+            playerPrefabs[3].gameObject.SetActive(false);
+            playerPrefabs[4].gameObject.SetActive(false);
+            playerPrefabs[5].gameObject.SetActive(false);
+
+            playerTokens[0].gameObject.SetActive(true);
+            playerTokens[1].gameObject.SetActive(true);
+            playerTokens[2].gameObject.SetActive(false);
+            playerTokens[3].gameObject.SetActive(false);
+            playerTokens[4].gameObject.SetActive(false);
+            playerTokens[5].gameObject.SetActive(false);
+        }
+        if (totalPlayers == 3)
+        {
+            playerPrefabs[0].gameObject.SetActive(true);
+            playerPrefabs[1].gameObject.SetActive(true);
+            playerPrefabs[2].gameObject.SetActive(true);
+            playerPrefabs[3].gameObject.SetActive(false);
+            playerPrefabs[4].gameObject.SetActive(false);
+            playerPrefabs[5].gameObject.SetActive(false);
+
+            playerTokens[0].gameObject.SetActive(true);
+            playerTokens[1].gameObject.SetActive(true);
+            playerTokens[2].gameObject.SetActive(true);
+            playerTokens[3].gameObject.SetActive(false);
+            playerTokens[4].gameObject.SetActive(false);
+            playerTokens[5].gameObject.SetActive(false);
+        }
+        if (totalPlayers == 4)
+        {
+            playerPrefabs[0].gameObject.SetActive(true);
+            playerPrefabs[1].gameObject.SetActive(true);
+            playerPrefabs[2].gameObject.SetActive(true);
+            playerPrefabs[3].gameObject.SetActive(true);
+            playerPrefabs[4].gameObject.SetActive(false);
+            playerPrefabs[5].gameObject.SetActive(false);
+
+            playerTokens[0].gameObject.SetActive(true);
+            playerTokens[1].gameObject.SetActive(true);
+            playerTokens[2].gameObject.SetActive(true);
+            playerTokens[3].gameObject.SetActive(true);
+            playerTokens[4].gameObject.SetActive(false);
+            playerTokens[5].gameObject.SetActive(false);
+        }
+        if (totalPlayers == 5)
+        {
+            playerPrefabs[0].gameObject.SetActive(true);
+            playerPrefabs[1].gameObject.SetActive(true);
+            playerPrefabs[2].gameObject.SetActive(true);
+            playerPrefabs[3].gameObject.SetActive(true);
+            playerPrefabs[4].gameObject.SetActive(true);
+            playerPrefabs[5].gameObject.SetActive(false);
+
+            playerTokens[0].gameObject.SetActive(true);
+            playerTokens[1].gameObject.SetActive(true);
+            playerTokens[2].gameObject.SetActive(true);
+            playerTokens[3].gameObject.SetActive(true);
+            playerTokens[4].gameObject.SetActive(true);
+            playerTokens[5].gameObject.SetActive(false);
+        }
+        if (totalPlayers == 6)
+        {
+            playerPrefabs[0].gameObject.SetActive(true);
+            playerPrefabs[1].gameObject.SetActive(true);
+            playerPrefabs[2].gameObject.SetActive(true);
+            playerPrefabs[3].gameObject.SetActive(true);
+            playerPrefabs[4].gameObject.SetActive(true);
+            playerPrefabs[5].gameObject.SetActive(true);
+
+            playerTokens[0].gameObject.SetActive(true);
+            playerTokens[1].gameObject.SetActive(true);
+            playerTokens[2].gameObject.SetActive(true);
+            playerTokens[3].gameObject.SetActive(true);
+            playerTokens[4].gameObject.SetActive(true);
+            playerTokens[5].gameObject.SetActive(true);
+        }
     }
+
 }
